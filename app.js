328525a7a1c44fc92c7cb0e2477e6e0dd259c109
Matheus Piscioneri <mxczpiscioneri@gmail.com>
2017-03-01 16:07:41 -0300

    Redirect all to /public/index.html

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();
 
 // Config
 app.use(express.static(__dirname + '/public'));
+app.get('/*', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 
 // Server
